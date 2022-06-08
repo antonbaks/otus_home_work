af681faff9b1431a856ed55c4fa836169b478201
anton <dr.baks.777@gmail.com>
2022-06-08 22:01:55 +0300

    HW08 is completed

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,57 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	envName := "NEW"
+	cmd := []string{"echo", ""}
+
+	t.Run("check change env", func(t *testing.T) {
+		os.Setenv(envName, "old")
+
+		returnCode := RunCmd(cmd, Environment{
+			envName: EnvValue{
+				Value:      "new",
+				NeedRemove: false,
+			},
+		})
+
+		require.Equal(t, "new", os.Getenv(envName))
+		require.Equal(t, 0, returnCode)
+	})
+
+	t.Run("check del env", func(t *testing.T) {
+		os.Setenv(envName, "old")
+
+		returnCode := RunCmd(cmd, Environment{
+			envName: EnvValue{
+				Value:      "",
+				NeedRemove: true,
+			},
+		})
+
+		_, ok := os.LookupEnv(envName)
+
+		require.False(t, ok)
+		require.Equal(t, 0, returnCode)
+	})
+
+	t.Run("check create env", func(t *testing.T) {
+		returnCode := RunCmd(cmd, Environment{
+			envName: EnvValue{
+				Value:      "old",
+				NeedRemove: false,
+			},
+		})
+
+		_, ok := os.LookupEnv(envName)
+
+		require.True(t, ok)
+		require.Equal(t, 0, returnCode)
+	})
 }

edcf8aacd22fae79b8589eb5d5593414aa584782
anton <dr.baks.777@gmail.com>
2022-06-08 21:18:05 +0300

    HW07 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	pathFileTo := "out.txt"
+
+	c := Copy("/dev/urandom", pathFileTo, 0, 0)
+
+	require.Truef(t, errors.Is(c, ErrUnsupportedFile), "actual error %q", c)
+
+	c = Copy("testdata/input.txt", pathFileTo, -1, 0)
+
+	require.Truef(t, errors.Is(c, ErrSeek), "actual error %q", c)
+
+	c = Copy("testdata/input.txt", pathFileTo, 10000, 0)
+
+	require.Truef(t, errors.Is(c, ErrOffsetExceedsFileSize), "actual error %q", c)
+
+	c = Copy("testdata/input.txt", pathFileTo, 0, 99999999)
+
+	require.Equal(t, c, nil)
+
+	_ = os.Remove(pathFileTo)
 }

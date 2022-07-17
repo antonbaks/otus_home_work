777f218826eaad6da365afd59cb8479d947a056f
anton <dr.baks.777@gmail.com>
2022-07-17 11:03:21 +0300

    HW10 is completed

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,11 +1,10 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
+	"github.com/mailru/easyjson"
 	"io"
-	"io/ioutil"
-	"regexp"
 	"strings"
 )
 
@@ -32,18 +31,17 @@ func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 type users [100_000]User
 
 func getUsers(r io.Reader) (result users, err error) {
-	content, err := ioutil.ReadAll(r)
-	if err != nil {
-		return
-	}
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
 
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
+	i := 0
+	for scanner.Scan() {
 		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
+		if err = easyjson.Unmarshal(scanner.Bytes(), &user); err != nil {
 			return
 		}
 		result[i] = user
+		i++
 	}
 	return
 }
@@ -52,12 +50,7 @@ func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 
 	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
-		}
-
-		if matched {
+		if strings.HasSuffix(user.Email, "."+domain) {
 			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
 			num++
 			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
